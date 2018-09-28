9dc8bc5298edd7e93937ea3b819a343ea7e194a5
Ashabrai Frauen <ashabraifrauen@yahoo.com>
2018-09-27 17:32:13 -0700

    fixed code and test

--- ll_kth_from_end/__test__/linked-list.test.js
+++ ll_kth_from_end/__test__/linked-list.test.js
@@ -3,22 +3,22 @@
 const LinkedList = require('../linked-list');
 
 const exampleList = new LinkedList();
+const emptyList = new LinkedList();
 
 for (let i = 0; i < 5; i++) {
   exampleList.insertAtHead(i);
 }
 
 describe('linked-list.js', () => {
-  test('value is not a number', () => {
-    const testValue = exampleList.reverseSearchList('b');
-    expect(testValue).toEqual();
-  });
   test('Success, its passed', () => {
     const testValue = exampleList.reverseSearchList(2);
-    expect(testValue).toEqual(2);
+    expect(testValue).toEqual(3);
   });
-  test('Throw error if value is bigger than list', () => {
-    const testValue = exampleList.reverseSearchList(10);
-    expect(testValue).toEqual('Exception');
+  test('If list is empty', () => {
+    try {
+      emptyList.reverseSearchList('value new');
+    } catch (error) {
+      expect(error.message).toEqual('_ERROR_ list empty');
+    }
   });
 });
